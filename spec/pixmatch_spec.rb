4c8ae7cf2ba92e162a32b45449bb30bc3c8c3fe6
dblock <dblock@dblock.org>
2011-04-13 16:56:20 -0400

    Externalized credentials into a pixmatch.yml.

--- spec/pixmatch_spec.rb
+++ spec/pixmatch_spec.rb
@@ -1,11 +1,17 @@
+require 'yaml'
+
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Pixmatch" do
   before(:each) do
     Pixmatch.configure do |config|
-      config.username = "username"
-      config.password = "password"
-      config.endpoint = "http://<yourserverhere>.tineye.com"
+      pixmatch_yml = File.join(File.dirname(__FILE__), '../config/pixmatch.yml')
+      pixmatch_yml = File.join(File.dirname(__FILE__), '../config/pixmatch.yml.default') if ! File.exists?(pixmatch_yml)
+      raise "missing config/pixmatch.yml" if ! File.exists?(pixmatch_yml) 
+      pixmatch_config = YAML.load_file(pixmatch_yml)['test']
+      config.username = pixmatch_config['username']
+      config.password = pixmatch_config['password']
+      config.endpoint = pixmatch_config['endpoint']
     end
   end
   describe "methods" do
