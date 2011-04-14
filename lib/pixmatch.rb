31a2ea9f50a0c1d9edd2c8909a2b940c73c4cfe2
Daniel Doubrovkine <dblock@dblock.org>
2011-04-13 21:27:47 -0400

    Replaced tabs with spaces.

--- lib/pixmatch.rb
+++ lib/pixmatch.rb
@@ -8,23 +8,23 @@ require File.expand_path('../pixmatch/client', __FILE__)
 # Adapted from the Ruby Twitter gem.
 # @see https://github.com/jnunemaker/twitter
 module Pixmatch
-	extend Configuration
-	
-	# Alias for pixmatch::Client.new
-	#
-	# @return {pixmatch::Client}
-	def self.client(options = {})
-		Pixmatch::Client.new(options)
-	end
-	
-	# Delegate to pixmatch::Client
-	def self.method_missing(method, *args, &block)
-		return super unless client.respond_to?(method)
-		client.send(method, *args, &block)
-	end
-	
-	# Delegate to pixmatch::Client
-	def self.respond_to?(method)
-		return client.respond_to?(method) || super
-	end
+  extend Configuration
+  
+  # Alias for pixmatch::Client.new
+  #
+  # @return {pixmatch::Client}
+  def self.client(options = {})
+    Pixmatch::Client.new(options)
+  end
+  
+  # Delegate to pixmatch::Client
+  def self.method_missing(method, *args, &block)
+    return super unless client.respond_to?(method)
+    client.send(method, *args, &block)
+  end
+  
+  # Delegate to pixmatch::Client
+  def self.respond_to?(method)
+    return client.respond_to?(method) || super
+  end
 end
--- lib/pixmatch/request.rb
+++ lib/pixmatch/request.rb
@@ -2,27 +2,27 @@ require 'uri'
 require 'rest_client'
 
 module Pixmatch
-	module Request		
-		private
-		
-		def request(http_method, path, query_params = {}, data_params = {})
-		  capture RestClient::Request.new({ 
-		    method: http_method,
-		    url: "#{endpoint}/#{paramify(path, query_params)}", 
-		    user: username,
-		    password: password
-		   }.merge(data_params)).execute   			
-		end
-		
-		def capture(response)
-			json = Utils.parse_json(response)
-		  Utils.handle_error(json)
-			json
-		end
-		
-		def paramify(path, params)
+  module Request		
+    private
+    
+    def request(http_method, path, query_params = {}, data_params = {})
+      capture RestClient::Request.new({ 
+        method: http_method,
+        url: "#{endpoint}/#{paramify(path, query_params)}", 
+        user: username,
+        password: password
+       }.merge(data_params)).execute   			
+    end
+    
+    def capture(response)
+      json = Utils.parse_json(response)
+      Utils.handle_error(json)
+      json
+    end
+    
+    def paramify(path, params)
       URI.encode("#{path}/?#{params.map { |k,v| "#{k}=#{v}" }.join('&')}")
     end
     
-	end
+  end
 end
